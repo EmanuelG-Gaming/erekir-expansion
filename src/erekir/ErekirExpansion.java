bad7e9a23a5a878bc0eea9c339239e831c004b35
EmanuelG-Gaming <emanuelgidea2000@gmail.com>
2022-05-20 21:24:59 +0300

    quick head start

---
+++ src/erekir/ErekirExpansion.java
@@ -0,0 +1,23 @@
+package erekir;
+
+import arc.*;
+import arc.util.*;
+import mindustry.*;
+import mindustry.content.*;
+import mindustry.game.EventType.*;
+import mindustry.gen.*;
+import mindustry.mod.*;
+import mindustry.ui.dialogs.*;
+
+public class ErekirExpansion extends Mod{
+
+    public ErekirExpansion() {
+        Log.info("Loaded Erekir buoyancy");
+    }
+
+    @Override
+    public void loadContent() {
+        Log.info("Loading some other erekir content.");
+    }
+
+}
