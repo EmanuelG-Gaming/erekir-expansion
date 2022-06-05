2ce031cdc8b4319c912a8c3b9291ed354d841606
EmanuelG-Gaming <emanuelgidea2000@gmail.com>
2022-06-05 16:51:02 +0300

    spread sprites

--- src/erekir/content/ErkTechTree.java
+++ src/erekir/content/ErkTechTree.java
@@ -8,8 +8,6 @@ import static mindustry.content.TechTree.*;
 
 //TODO name
 public class ErkTechTree{
-   static TechTree.TechNode context = null;
-   
    public static void load() {
        node(mechFabricator, () -> {
            node(ErkUnitTypes.gem);
