d23648de22cec2920ff4891e925838f146692b38
EmanuelG-Gaming <emanuelgidea2000@gmail.com>
2022-06-14 13:58:57 +0300

    static

--- src/erekir/util/ErkUtil.java
+++ src/erekir/util/ErkUtil.java
@@ -2,13 +2,15 @@ package erekir.util;
 
 import arc.func.*;
 import arc.math.geom.*;
+import mindustry.game.*;
+import mindustry.game.Teams.*;
 import mindustry.gen.*;
 import erekir.world.blocks.environment.ItemProp.*;
 
 public class ErkUtil{
    
    /** Iterates over every building and checks if it's a drop build. */
-   public void allDrops(Cons<ItemProp.ItemBuild> e) {
+   public static void allDrops(Cons<ItemProp.ItemBuild> e) {
       for (Building build : Groups.build) {
          if (build instanceof ItemProp.DropBuild) {
             DropBuild drop = (ItemProp.ItemBuild) build;
@@ -18,10 +20,10 @@ public class ErkUtil{
    }
    
    /* Iterates over every building of a team and checks if they're within a certain range. */
-   public void dropsWithin(Team team, float x, float y, float range, Cons<ItemProp.ItemBuild> e) {
+   public static void dropsWithin(Team team, float x, float y, float range, Cons<ItemProp.ItemBuild> e) {
       for (Building build : Groups.build) {
          if (build instanceof ItemProp.DropBuild) {
-            DropBuild drop = (ItemProp.ItemBuild) build;
+            DropBuild drop = (ItemProp.DropBuild) build;
             
             if (drop.team == team && drop.within(x, y, range)) {
                e.get(drop);
@@ -30,10 +32,10 @@ public class ErkUtil{
       } 
    }
    
-   public void dropsWithin(Team team, Position pos, float range, Cons<ItemProp.ItemBuild> e) {
+   public static void dropsWithin(Team team, Position pos, float range, Cons<ItemProp.DropBuild> e) {
       for (Building build : Groups.build) {
          if (build instanceof ItemProp.DropBuild) {
-            DropBuild drop = (ItemProp.ItemBuild) build;
+            DropBuild drop = (ItemProp.DropBuild) build;
             
             if (drop.team == team && drop.within(pos.x, pos.y, range)) {
                e.get(drop);
@@ -42,16 +44,16 @@ public class ErkUtil{
       } 
    }
    
-   public boolean dropWithin(ItemProp.DropBuild build, float x, float y, float range) {
+   public static boolean dropWithin(ItemProp.DropBuild build, float x, float y, float range) {
        return build.within(x, y, range);
    }
    
-   public boolean dropWithin(ItemProp.DropBuild build, Position pos, float range) {
-       return dropWithin(build, pos.x, pos.y, range)
+   public static boolean dropWithin(ItemProp.DropBuild build, Position pos, float range) {
+       return dropWithin(build, pos.x, pos.y, range);
    }
    
    /* Whenever a building has the overlay button. */
-   public boolean containsButton(ItemProp.DropBuild build) {
+   public static  boolean containsButton(ItemProp.DropBuild build) {
        return build.containsButton;
    }
 }
