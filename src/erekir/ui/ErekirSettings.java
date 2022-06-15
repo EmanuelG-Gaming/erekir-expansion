b0ead6d7354321184ad8bef866060e992f84ae5d
EmanuelG-Gaming <emanuelgidea2000@gmail.com>
2022-06-15 09:24:36 +0300

    separate dialog tm

--- src/erekir/ui/ErekirSettings.java
+++ src/erekir/ui/ErekirSettings.java
@@ -1,37 +1,14 @@
 package erekir.ui;
 
 import arc.Core;
-import arc.scene.*;
-import arc.scene.ui.layout.*;
-import arc.scene.style.*;
-import mindustry.graphics.*;
-import mindustry.ui.*;
-import mindustry.gen.*;
+import erekir.ui.dialogs.*;
 
 import static mindustry.Vars.*;
 
 public class ErekirSettings{
-   private static float buttonW = 48f, buttonH = 48f;
-   private static int rowCount = 6;
-   
    public static void load() {
       ui.settings.addCategory("Erekir expansion", "erekir-expansion-gem-full", t -> {
-          t.defaults().padBottom(8);
-          t.add("Button icons (requires restart)").color(Pal.accent).row();
-          
-          t.pane(Styles.defaultPane, t2 -> {
-             //row indice
-             int[] r = {0}; 
-             Icon.icons.each((name, icon) -> {
-                if (!Core.settings.getBool("erekir-expansion-displaySmall") && name.contains("small")) return;
-                t2.button(new TextureRegionDrawable(icon), Styles.cleari, () -> {
-                    Core.settings.put("erekir-expansion-buttonIcon", name);
-                    ui.showInfo("Changing the button icon to " + name + ".");
-                }).size(buttonW, buttonH).margin(4f).pad(0f);
-                
-                if (++r[0] % rowCount == 0) t2.row();
-             });
-          }).size(buttonW * rowCount + 6f, buttonH * 25f + 6f);
+          t.button("Button icons", () -> ButtonIconsDialog.show()).size(280f, 60f);
           
           //also display small images
           t.checkPref(
---
+++ src/erekir/ui/dialogs/ButtonIconsDialog.java
@@ -0,0 +1,38 @@
+package erekir.ui.dialogs;
+
+import arc.Core;
+import arc.scene.*;
+import arc.scene.ui.layout.*;
+import arc.scene.style.*;
+import mindustry.graphics.*;
+import mindustry.ui.*;
+import mindustry.ui.dialogs.*;
+
+import static mindustry.Vars.*;
+
+public class ButtonIconsDialog extends BaseDialog{
+   private static float buttonW = 48f, buttonH = 48f;
+   private static int rowCount = 6;
+   
+   public ButtonIconsDialog() {
+      super(Core.bundle.get("erekir-expansion-buttonIcons"));
+      addCloseButton();
+      
+      cont.add("Button icons (requires restart)").color(Pal.accent).padBottom(8f).row();
+          
+      cont.pane(Styles.defaultPane, t -> {
+         //row indice
+         int[] r = {0}; 
+         Icon.icons.each((name, icon) -> {
+            if (!Core.settings.getBool("erekir-expansion-displaySmall") && name.contains("small")) return;
+            
+            t.button(new TextureRegionDrawable(icon), Styles.cleari, () -> {
+               Core.settings.put("erekir-expansion-buttonIcon", name);
+               ui.showInfo("Changing the button icon to " + name + ".");
+            }).size(buttonW, buttonH).margin(4f).pad(2f);
+            
+            if (++r[0] % rowCount == 0) t.row();
+         });
+      }).size(buttonW * rowCount + 6f, buttonH * 25f + 6f);
+   }
+}
