dd0cd76dee62e8313ce7fda74f2e0b8160534585
EmanuelG-Gaming <emanuelgidea2000@gmail.com>
2022-06-23 09:43:23 +0300

    "why aren't my units circle?"

--- src/erekir/ErekirExpansion.java
+++ src/erekir/ErekirExpansion.java
@@ -70,19 +70,20 @@ public class ErekirExpansion extends Mod{
         
         Events.run(Trigger.draw, () -> {
            for (Unit unit : Groups.unit) {
-              if (unit.isFlying() || unit.hovering) return;
-              
+             
               Floor floor = unit.tileOn() == null ? Blocks.air.asFloor() : unit.tileOn().floor();
               if (floor.isLiquid && floor == ErkBlocks.angryArkyciteFloor) {
-                 float z = Draw.z(); 
+                 if (!unit.isFlying() || unit.hovering == false) {
+                    float z = Draw.z(); 
                   
-                 Draw.z(Layer.debris);
-                 Draw.color(Tmp.c1.set(floor.mapColor).mul(1.5f));
-                 Lines.stroke(4f);
-                 Lines.circle(unit.x, unit.y, unit.type.hitSize * 1.25f);
-                 Draw.reset();
+                    Draw.z(Layer.debris);
+                    Draw.color(Tmp.c1.set(floor.mapColor).mul(1.5f));
+                    Lines.stroke(4f);
+                    Lines.circle(unit.x, unit.y, unit.type.hitSize * 1.25f);
+                    Draw.reset();
                  
-                 Draw.z(z);
+                    Draw.z(z);
+                 }
               }
            }
         });
