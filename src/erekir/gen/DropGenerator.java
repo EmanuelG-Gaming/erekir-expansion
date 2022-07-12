8a575708ec7c78feb32f5b04e4034c49d58a1553
EmanuelG-Gaming <emanuelgidea2000@gmail.com>
2022-07-12 14:44:34 +0300

    map update

--- src/erekir/gen/DropGenerator.java
+++ src/erekir/gen/DropGenerator.java
@@ -10,7 +10,18 @@ import static mindustry.Vars.*;
 /** Generates drop builds based on the current non-hidden items. */
 public class DropGenerator{
    private static ItemProp last;
-   private static Seq<ItemProp> generated = new Seq<ItemProp>();
+   private static int[content.items().size] amounts = [
+      4, 4, 3,
+      5, 5, 3,
+      1, 3, 4,
+      3, 1, 1,
+      2, 3, 2,
+      4, 3, 2,
+      1
+   ];
+   
+   /** Contains only drops with shown vanilla items! */
+   public static Seq<ItemProp> generated = new Seq<ItemProp>();
    
    //funny generating code
    public static void generateDrops() {
@@ -27,11 +38,22 @@ public class DropGenerator{
             generated.add(last);
          }
       }
+      setToDrop();
+   }
+
+   public static void setToDrop() {
+      generated.each(b -> {
+         setAmount(b, amounts[b.id]);
+      });
    }
    
    public static void handleIcons() {
       generated.each(b -> {
-         b.region = Core.atlas.find("item-" + b.dropItem.name);
+         b.region = b.dropItem.fullIcon
       });
+   }
+   
+   private static void setAmount(ItemProp drop, int amount) {
+      drop.amount = amount;
    }
 }
