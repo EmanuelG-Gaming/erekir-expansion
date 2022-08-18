d94158045ceba548c34149baf76b6a06987e0d4c
EmanuelG-Gaming <emanuelgidea2000@gmail.com>
2022-08-18 10:00:33 +0300

    back

--- src/erekir/ErkVars.java
+++ src/erekir/ErkVars.java
@@ -1,12 +1,16 @@
 package erekir;
 
 import arc.struct;
+import arc.Events;
+import mindustry.game.EventType.*;
 import erekir.room;
 
 public class ErkVars{
-   public Seq<BaseRoom> rooms = new Seq<BaseRoom>;
+   public Seq<BaseRoom> rooms = new Seq<>();
    
    public void load() {
-      rooms.clear();
+      Events.on(WorldLoadEvent.class, e -> {
+         rooms.clear();
+      });
    }
 }
